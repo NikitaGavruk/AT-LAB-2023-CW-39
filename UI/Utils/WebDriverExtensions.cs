74c9b9cabb4087450e75fe336b3b22dd3f5b5675
Evgen <dapodobral@gmail.com>
2023-07-27 11:24:04 +0300

    temp commit

---
+++ UI/Utils/WebDriverExtensions.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using UI.WebDriver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Utils
+{
+    public static class WebDriverExtensions
+    {    
+        public static void ClickOnElement(By locator, int waitSeconds)
+        {
+            IWebDriver driver = Browser.GetDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitSeconds));
+            wait.Until(d => d.FindElement(locator).Displayed);
+            driver.FindElement(locator).Click();
+        }
+
+        public static void ClickOnElement(By locator)
+        {
+            IWebDriver driver = Browser.GetDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => d.FindElement(locator).Displayed);
+            driver.FindElement(locator).Click();
+        }
+
+        public static void ClickOnEnter(By locator)
+        {
+            IWebDriver driver = Browser.GetDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(d => d.FindElement(locator).Displayed);
+            driver.FindElement(locator).SendKeys(Keys.Enter);
+        }
+
+        public static string GetTextFromElement(By locator, int waitSeconds)
+        {
+            IWebDriver driver = Browser.GetDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(waitSeconds));
+
+        }
+    }
+}
