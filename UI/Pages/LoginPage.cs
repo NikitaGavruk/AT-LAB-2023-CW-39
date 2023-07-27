60d8cfee683636230338b59f8f8d6d03ed49de78
Evgen <dapodobral@gmail.com>
2023-07-27 17:34:34 +0300

    add login page

---
+++ UI/Pages/LoginPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Pages
+{
+    internal class LoginPage
+    {
+        private static readonly By usernameField = By.CssSelector("#wpName1");
+        private static readonly By passwordField = By.CssSelector("#wpPassword1");
+        private static readonly By rememberMeCheckbox = By.CssSelector("#wpRemember");
+        private static readonly By loginButton = By.CssSelector("#wpLoginAttempt");
+    }
+}
