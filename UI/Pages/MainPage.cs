fb532e91818fb7eb0c311f6a5759b3d2b29a9131
Evgen <dapodobral@gmail.com>
2023-07-27 17:27:52 +0300

    add main page locators

---
+++ UI/Pages/MainPage.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Pages
+{
+    public class MainPage
+    {
+        private static readonly By searchField = By.CssSelector("[name='search']");
+        private static readonly By loginButton = By.CssSelector("#pt-login-2>a>span");
+        private static readonly By openSideMenuButton = By.CssSelector("#vector-main-menu-dropdown-checkbox");
+        private static readonly By randomPageButton = By.CssSelector("#n-randompage>a>span");
+    }
+}
