f1d153384ad813e0277ac528081c7f1fea9b7ff4
Evgen <dapodobral@gmail.com>
2023-07-27 17:38:44 +0300

    update main page

--- UI/Pages/MainPage.cs
+++ UI/Pages/MainPage.cs
@@ -1,4 +1,5 @@
 ﻿using OpenQA.Selenium;
+using UI.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,11 @@ namespace UI.Pages
         private static readonly By loginButton = By.CssSelector("#pt-login-2>a>span");
         private static readonly By openSideMenuButton = By.CssSelector("#vector-main-menu-dropdown-checkbox");
         private static readonly By randomPageButton = By.CssSelector("#n-randompage>a>span");
+        private static readonly By title = By.CssSelector("#Welcome_to_Wikipedia");
+
+        public bool IsPageVisible()
+        {
+            return WebDriverExtension.IsElementVisible(title, 5);
+        }
     }
 }
