b29e90ababf8da6218922ee60267f642b800a72a
Sirojbek Azimjonov <joris5969@gmail.com>
2023-07-31 12:59:51 +0500

    BaseTest Initial

---
+++ UI/Tests/BaseTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+using System;
+using UI.WebDriver;
+using OpenQA.Selenium;
+
+namespace UI.Tests
+{
+	public class BaseTest
+	{
+		protected static IWebDriver Driver;
+
+
+		[SetUp]
+		public void Setup()
+		{
+
+			Driver = Browser.GetDriver();
+			Browser.WindowMaximaze();
+			Browser.StartNavigate();
+		}
+
+		[TearDown]
+		public void Quite()
+		{
+			Browser.QuiteBrowser();
+		}
+
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+		}
+	}
+}
