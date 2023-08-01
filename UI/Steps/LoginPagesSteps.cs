e8d03a5cef5c4e4a36cddd96151c7ca08da449ea
Evgen <dapodobral@gmail.com>
2023-08-01 10:18:36 +0300

    Separate steps

--- UI/Steps/LoginPagesSteps.cs
+++ UI/Steps/LoginPagesSteps.cs
@@ -7,28 +7,15 @@ using UI.Pages;
 
 namespace UI.Steps
 {
-    public static class Steps
+    public class LoginPagesSteps
     {
         private static LoginPage loginPage = new LoginPage();
-        private static MainPage mainPage = new MainPage();
 
         public static void Login(string username, string password, int timeSeconds)
         {
             loginPage.EnterUsername(username, timeSeconds);
             loginPage.EnterPassword(password, 1);
             loginPage.ClickToLoginButton(1);
-        }
-
-        public static void Logout(int timeSeconds)
-        {
-            mainPage.CallUserDropdownMenu(timeSeconds);
-            mainPage.ClickToLogout(2);
-        }
-
-        public static void Search(string searchText, int timeSeconds)
-        {
-            mainPage.SendKeysToSearchField(searchText, timeSeconds);
-            mainPage.ClickToSearchButton(1);
         }
     }
 }
