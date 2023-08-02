b8219450b5faf3a8f011cb64a9db47923db8da7b
Evgen <dapodobral@gmail.com>
2023-08-02 10:49:11 +0300

    update test

--- UI/Tests/Test.cs
+++ UI/Tests/Test.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Core;
+using NUnit.Framework;
 using UI.Pages;
 using UI.Steps;
 
@@ -7,9 +8,7 @@ namespace UI.Tests
 	[TestFixture]
 	public class Test : BaseTest
 	{
-        private static MainPage mainPage = new MainPage();
-        private static string username = "AliceTestAcc4587";
-        private static string password = "#AlicePassword";
+        private static MainPage mainPage = new MainPage();        
 
         [Test]
         public void FindArticle()
@@ -29,10 +28,10 @@ namespace UI.Tests
             CustomLogger.LogInfo(Utils.LogLevel.Info, "Go to Login page");
             mainPage.ToLoginPage();
             CustomLogger.LogInfo(Utils.LogLevel.Info, "Login to account");
-            string actualUsername = LoginPagesSteps.Login(username, password)
+            string actualUsername = LoginPagesSteps.Login()
             .GetLoggedUsername();
             CustomLogger.LogInfo(Utils.LogLevel.Info, "Verify username is right");
-            Assert.That(actualUsername, Is.EqualTo(username));
+            Assert.That(actualUsername, Is.EqualTo(TestDataReader.GetTestUsername()));
 
             CustomLogger.LogInfo(Utils.LogLevel.Info, "Click to random article");
             bool IsEditPageVisible = mainPage.ClickToRandomArticle()
