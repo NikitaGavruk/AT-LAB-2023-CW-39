67e4de6fad76db133cb7fb99cff5ff2de375091e
Evgen <dapodobral@gmail.com>
2023-08-09 13:13:53 +0300

    Add GetArticlePdfRequest test

---
+++ API/Tests/Tests.cs
@@ -0,0 +1,31 @@
+﻿using API.APIUtils;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Linq;
+using System.Net;
+using Core.enums;
+
+namespace API.Tests
+{
+    [TestFixture]
+    public class Tests : BaseTest
+    {
+        [Test]
+        public void GetArticlePdfRequest()
+        {
+            string url = "https://en.wikipedia.org/api/rest_v1/page/pdf/Mikhail_Lomonosov";
+            string expectedContentType = "application/pdf";
+
+            logger.LogInfo(LogLevel.Info, $"Create GET request taking the endpoint {url}");
+            RestRequest request = api.CreateGetRequest(url, ("accept", "application/pdf"));
+            logger.LogInfo(LogLevel.Info, "Send the request and get the response");
+            RestResponse response = api.GetResponse(request);
+            string actualContentType = response.ContentType;
+
+            logger.LogInfo(LogLevel.Info, "Check if the response content is compare to expected");
+            Assert.That(actualContentType, Is.EqualTo(expectedContentType));
+        }
+    }
+}
