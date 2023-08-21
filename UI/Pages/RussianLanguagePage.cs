a5f266a28980a714c0d007e3b5fb0848adec5801
khayitmuradov <khayitmuradov@proton.me>
2023-08-21 08:14:55 +0500

    add title prop and GetTitle() method

--- UI/Pages/RussianLanguagePage.cs
+++ UI/Pages/RussianLanguagePage.cs
@@ -6,6 +6,12 @@ namespace UI.Pages
     public class RussianLanguagePage : AbstractPage
     {
         private static readonly By englishLanguageButton = By.CssSelector(".interwiki-en span");
+        private static readonly By title = By.XPath("//span[@class='mw-page-title-main']");
+
+        public string GetTitle()
+        {
+            return WebDriverExtension.GetTextFromElement(title);
+        }
 
         public bool EnglishLanguageButtonIsVisible()
         {
