b2e47236dfd3202927b20d3333f1d9ca7c923e0e
Evgen <dapodobral@gmail.com>
2023-08-25 13:14:50 +0300

    initial test

---
+++ AndroidUI/Tests/Tests.cs
@@ -0,0 +1,36 @@
+﻿using AndroidUI.Driver;
+using AndroidUI.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Support.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidUI.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private AndroidDriver<IWebElement> driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = DriverFactory.GetDriver();
+        }
+
+        [Test]
+        public void SimpleTest()
+        {
+            DriverExtensions.PressBack();
+            DriverExtensions.ClickToElement(By.CssSelector("android.widget.TextView"));
+            DriverExtensions.SendKeys(By.CssSelector("android.widget.EditText"), "Mikhail Lomonosov");
+            driver.PressKeyCode(AndroidKeyCode.Keycode_ENTER);
+
+            Assert.Pass();
+        }
+    }
+}
