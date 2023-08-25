d025108ee9b37af3152515ac27b7187d95cb611b
Evgen <dapodobral@gmail.com>
2023-08-25 13:13:17 +0300

    Add extensions

--- AndroidUI/Utils/DriverExtensions.cs
+++ AndroidUI/Utils/DriverExtensions.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AndroidUI.Driver;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
 
 namespace AndroidUI.Utils
 {
     public class DriverExtensions
     {
-        private AndroidDriver
+        private static AndroidDriver<IWebElement> driver => DriverFactory.GetDriver();
+
+        public static void ClickToElement(By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
+
+        public static void SendKeys(By locator, string keys)
+        {
+            driver.FindElement(locator).SendKeys(keys);
+        }
+
+        public static string GetText(By locator) => driver.FindElement(locator).Text;
+
+        public static void PressBack()
+        {
+            driver.Navigate().Back();
+        }
+
+        public static void PressKey(int AndroidKeyCode) 
+        {
+            //Use AndoidKeyCode.key to find key code
+            driver.PressKeyCode(AndroidKeyCode);
+        }
+
     }
 }
